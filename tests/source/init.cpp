a9e6f3e559a84133527f674874f27f63f7a24fd2
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-03-30 13:09:31 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -44,7 +44,7 @@ SCENARIO("Matrix *", "[multiplying]") {
 	Matrix B = Matrix(2,2);
 	B.Zapoln("B2x2.txt");
 	Matrix expected = Matrix(2,2);
-	expected.Zapoln("AxB2x2.txt")
+	expected.Zapoln("AxB2x2.txt");
 	Matrix result = A * B;
 	REQUIRE(result == expected);
 }
