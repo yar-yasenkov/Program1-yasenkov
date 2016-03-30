c2dc039824918b2d99ea22708866ad0539bdeeca
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-03-30 10:16:24 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -23,16 +23,16 @@ SCENARIO("Matrix init", "[init]") {
 
 SCENARIO("Matrix >>", "[fill]") {
 	Matrix A = Matrix(2, 2);
-	REQUIRE( A.fill("A2x2.txt") );
+	REQUIRE( A.Zapoln("A2x2.txt") );
 }
 
 SCENARIO("Matrix +", "[addition]") {
 	Matrix A = Matrix(2, 2);
-	A.fill("A2x2.txt");
+	A.Zapoln("A2x2.txt");
 	Matrix B = Matrix(2, 2);
-	B.fill("B2x2.txt");
+	B.Zapoln("B2x2.txt");
 	Matrix expected = Matrix(2, 2);
-	expected.fill("A+B2x2.txt");
+	expected.Zapoln("A+B2x2.txt");
 
 	Matrix result = A + B;
 	REQUIRE(result == expected);
