4359b737f0eaa2ea58a14ce39d2936ead9c2f3bb
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-04-16 20:44:35 +0300

    Update Matrix.hpp

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -205,6 +205,215 @@ public:
 	int kolstolb() const;
 };
 
+template <> class Matrix<char>
+{
+	private:
+	T **m;
+	int columns, rows;
+	friend istream& operator>>(istream& os, Matrix& object)
+	{
+		for (int i = 0; i < object.rows; i++)
+		{
+			for (int j = 0; j < object.columns; j++)
+			{
+				os >> object.m[i][j];
+			}
+		}
+		return os;
+	}
+	friend ostream& operator<<(ostream& os, Matrix& object)
+	{
+		for (int i = 0; i < object.rows; i++)
+		{
+			for (int j = 0; j < object.columns; j++)
+			{
+				os << object.m[i][j] << " ";
+			}
+			os << endl;
+		}
+		return os;
+	}
+public:
+
+	
+	class Badindex //класс исключений
+	{
+	public:
+		int badindex;
+		Badindex(int i) : badindex(i) {}
+		void Report() 
+		{
+			cout << "This value can not be negative" << badindex << endl;
+			return ;
+		}
+	        void Reportrow() 
+		{
+			cout << "This row does not exist" << endl;
+			return ;
+		}
+	};	
+	Matrix() :rows(0), columns(0), m(nullptr)
+	{}
+	Matrix(int kolstr, int kolstl) throw(Badindex &):
+		columns(kolstl), rows(kolstr) 
+	{
+		
+		
+			if ((kolstr < 0) || (kolstl < 0))
+			{
+				if (kolstl < 0)
+					throw Badindex(kolstl);
+				else
+					throw Badindex(kolstr);
+			}
+
+			m = new T *[rows];
+			for (int i = 0; i < rows; i++)
+			{
+				m[i] = new T[columns];
+				for (int j = 0; j < columns; j++)
+				{
+					m[i][j] = 0;
+				}
+			}
+
+	}
+
+	Matrix(int _rows, int _columns, int time) throw (Badindex &);
+        void Zapoln(string name)
+	{
+		//char fulpath[256] = "path";
+		string full_name;
+		full_name = name;
+		int  i,j;
+		T a;
+		fstream fin;
+		//strcat_s(fulpath, path);
+		fin.open(full_name, ios::in);//îòêðûòèå ôàéëà
+		if (fin.is_open())
+		{
+			for (int j = 0; j<rows; j++)
+			{
+				for (int i = 0; i < columns; i++)
+				{
+					fin >> a;
+					m[j][i] = a;
+				}
+
+			}
+		}
+		else
+		 {
+		    cout << "Îøèáêà, ïîïðîáóéòå åùå ðàç";
+	            exit(100);
+	}
+		fin.close();
+	}
+	
+	bool Show(int kolstr, int kolstl) const throw(Badindex &)
+	{
+	
+		bool mark;
+		
+		if ((kolstr < 0) || (kolstl < 0))
+		{
+			if (kolstl < 0)
+				throw Badindex(kolstl);
+			else
+				throw Badindex(kolstr);
+		}
+		for (int i = 0; i < kolstr; i++)
+		{
+			cout << endl;
+			for (int j = 0; j < kolstl; j++)
+			{
+                                mark=true;
+				cout << " " << m[i][j];
+			}
+		}
+		cout << endl;
+		
+		return mark;
+	}
+
+
+
+	Matrix & operator=(const Matrix & matrix) {
+		if (this != &matrix) { //перегруженный оператор присваивания
+			for (int i = 0; i < rows; i++)
+				delete[] m[i];
+			delete[] m;
+			columns = matrix.columns;
+			rows = matrix.rows;
+			m = new T *[rows];
+			for (int j = 0; j<rows; j++)
+			{
+				m[j] = new T[columns];
+				for (int i = 0; i < columns; i++)
+				{
+					m[j][i] = matrix.m[j][i];
+				}
+
+			}
+		}
+		return *this;
+	}
+
+
+        bool operator==(const Matrix &right) const// оператор сравнения
+       {
+          	if (rows != right.rows || columns != right.columns)
+    		return false; // матрицы с разным количеством элементов
+
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			if (m[i][j] != right.m[i][j])
+				return false; // матрицы не равны
+
+	return true; // матрицы равны
+        }
+
+
+
+
+	~Matrix()
+	{
+		if (m != nullptr)
+		{
+			for (int i = 0; i < rows; i++)
+			{
+				delete[] m[i];
+			}
+			delete[] m;
+		}
+	}
+
+
+
+	Matrix operator+ (const Matrix&) const
+	{
+	int a,b,r;
+	Matrix res(rows, columns);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+		        a=atoi(m[i][j]);
+		        b=atoi(a.m[i][j]);
+			r=a+b;
+			itoa(r,res.m[i][j],10);
+			//res.m[i][j] = m[i][j] + a.m[i][j];
+		}
+	}
+	return res;
+	}
+	Matrix operator*(const Matrix&) const;
+    T* operator[](int a) throw(Badindex &);
+	int kolstrok() const;
+	int kolstolb() const;
+};
+
+
 
 //конструктор случайной матрицы
 template <typename T>
